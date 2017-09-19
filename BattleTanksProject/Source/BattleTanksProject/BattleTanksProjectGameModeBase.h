aa6dc12a8cedda41ffdfc60651371f22369d2d5c
Romomomo <roman-winkler@gmx.at>
2017-09-19 15:06:28 +0200

    Setting up starting level.

---
+++ BattleTanksProject/Source/BattleTanksProject/BattleTanksProjectGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTanksProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKSPROJECT_API ABattleTanksProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
